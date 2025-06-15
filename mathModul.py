cee6eedb7bf884782b92aa20fd2ef740f6a2a49c
emreosminho <emreosmancanakci@gmail.com>
2025-06-15 11:22:27 +0300

    lesson start

---
+++ mathModul.py
@@ -0,0 +1,11 @@
+a = 5
+b = 4
+topla = a + b
+carp = a * b
+bol = a / b
+cikart = a - b
+print("Toplam:", topla)
+print("Carpim:", carp)
+print("Bolum:", bol)
+print("Cikartma:", cikart)
+print("bol değişkeninin tipi:", type(bol))
