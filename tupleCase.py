6124cb69f6195fa2dcbe1926d59e1c8d14c00443
testkateer <testkateer@gmail.com>
2025-06-27 22:30:48 +0300

    tuple

---
+++ tupleCase.py
@@ -0,0 +1,13 @@
+# tuple immutuble 
+
+x = 10
+y = 65
+
+konum = (10,65)
+print(konum[0])
+
+# farklı veri tipleri barındırabilir
+t = (10, y , 85, [5,6])
+print(t[3][1]) # liste varsa elemanı çağırılabilir.
+
+
