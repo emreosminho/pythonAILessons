ec7efa4388114ed5014e80147bad2c562b0e84fd
emreosminho <emreosmancanakci@gmail.com>
2025-06-28 10:14:08 +0300

    func

--- funcCase.py
+++ funcCase.py
@@ -10,3 +10,21 @@ def power(x,y):
 
 sonuc = power(2,3)
 print(sonuc)
+
+
+def fakHesapla(x):
+    fak = 1
+    for i in range(1, x + 1):
+        fak *= i
+    return fak
+
+print(fakHesapla(5))
+
+def fakHesapla(x):
+    if x < 0:
+        return "Negatif sayıların faktöriyeli yoktur."
+    elif x == 0 or x == 1:
+        return 1
+    else:
+        return x * fakHesapla(x - 1)
+print(fakHesapla(-1))
